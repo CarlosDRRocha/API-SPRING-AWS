302fc8b40845ac3eb9dab873e6eacb4f16355146
Carlos Daniel <danielrocha2134@gmail.com>
2025-04-06 13:13:48 -0300

    "Registering data in the event table and connecting with AWS."

---
+++ api/src/main/java/com/eventostec/api/service/EventService.java
@@ -0,0 +1,83 @@
+package com.eventostec.api.service;
+
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.repositories.EventRepository;
+
+import software.amazon.awssdk.annotations.NotNull;
+
+@Service
+public class EventService {
+
+  @Value("${aws.bucket.name}")
+  private String bucketName;
+
+
+  @Autowired
+  private AmazonS3 s3Client;
+
+  @Autowired
+  private EventRepository repository;
+
+
+  public Event createEvent(EventRequestDTO data){
+    String imgUrl = null;
+
+    if (data.image() != null) {
+      imgUrl = this.uploadImg(data.image());
+    }
+
+    Event newEvent = new Event();
+    newEvent.setTitle(data.tittle());
+    newEvent.setDescription(data.description());
+    newEvent.setEventUrl(data.eventUrl());
+    newEvent.setDate(new Date(data.date()));
+    newEvent.setImgUrl(imgUrl);
+    newEvent.setRemote(data.remote());
+    
+    repository.save(newEvent);
+
+    return newEvent;
+  }
+
+  private String uploadImg(MultipartFile multipartFile){
+    String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+   
+   
+    try {
+      File file = this.convertMultipartToFile(multipartFile);
+      s3Client.putObject(bucketName, fileName, file);
+      file.delete();
+      return s3Client.getUrl(bucketName, fileName).toString();
+    } catch (Exception e) {
+      System.out.println("Error uploading file to S3 ");
+      return "";
+    }
+   
+  }
+
+  private File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
+    File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+    FileOutputStream fos = new FileOutputStream(convFile);
+    fos.write(multipartFile.getBytes());
+    fos.close();
+    return convFile;
+  }
+}
+
+
